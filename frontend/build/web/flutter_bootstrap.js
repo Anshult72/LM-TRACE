955e5e621a96899ed93bffa44825185dcb8bb33b
Anshult72 <tripathianshul916@gmail.com>
2026-09-23 20:39:01 +0530

    fix(branding): ensure web assets, favicon and logo png files are properly deployed

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2143808411" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2411125410" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
